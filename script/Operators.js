8e71f05fe879d473129b71a40cd52c1f41d522e9
Kennedy Lajustra <kennedylajustra@gmail.com>
2020-06-09 22:28:59 +0700

    end of the day 9th june

---
+++ script/Operators.js
@@ -0,0 +1,49 @@
+var active = "all";
+function toggleDefender(){
+    if(active != "def"){
+        hideElement("atk_item");
+        showElement("def_item");
+        applyActiveButton("def_btn");
+        applyInactiveButton("atk_btn");
+        active = "def";
+    }
+    else if (active == "def"){
+        showElement("atk_item");
+        applyInactiveButton("def_btn");
+        active = "all";
+    }
+}
+function toggleAttacker(){
+    if(active != "atk"){
+        hideElement("def_item");
+        showElement("atk_item");
+        applyActiveButton("atk_btn");
+        applyInactiveButton("def_btn");
+        active = "atk";
+    }
+    else if (active == "atk"){
+        showElement("def_item");
+        applyInactiveButton("atk_btn");
+        active = "all";
+    }
+}
+function hideElement(id){
+    var item = document.getElementById(id);
+    item.style.display = "none";
+}
+function showElement(id){
+    var item = document.getElementById(id);
+    item.style.display = "inline-block";
+}
+function applyActiveButton(id){
+    var  b = document.getElementById(id);
+    b.style.backgroundColor = "white";
+    b.style.color = "black";
+    b.style.borderColor = "white";
+}
+function applyInactiveButton(id){
+    var b = document.getElementById(id);
+    b.style.backgroundColor = "#23242d";
+    b.style.borderColor = "#3f4353";
+    b.style.color= "white";
+}
